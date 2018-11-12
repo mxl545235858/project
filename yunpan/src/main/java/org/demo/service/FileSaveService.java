a1168f8fcb75cad4955f22225ecff89c3d78be7c
123 <545235858@qq.com>
2018-11-12 21:57:00 +0800

    1

--- yunpan/src/main/java/org/demo/service/FileSaveService.java
+++ yunpan/src/main/java/org/demo/service/FileSaveService.java
@@ -1,5 +1,40 @@
+
 package org.demo.service;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.stereotype.Service;
+@Service
 public class FileSaveService {
+	
+	public void FileReader(String fileName) {
+		File file = new File(fileName);	
+		InputStream in = null;
+		try {
+			in = new FileInputStream(file);	
+			FileReader filer = new FileReader(fileName);
+			BufferedReader br = new BufferedReader(filer);
+//			int tempbyte;  
+//			String s;
+			System.out.println(file.length());
+//			while ((s = br.readLine()) != null) {  
+//				System.out.print(s);  
+//            }  
+//			 
+//            while ((tempbyte = in.read()) != -1) {  
+//                System.out.write(tempbyte);  
+//            }  
+            in.close();  
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} 
+	}
 
 }
---
+++ yunpan/src/main/java/org/demo/test/test.java
@@ -0,0 +1,12 @@
+package org.demo.test;
+
+import org.demo.service.FileSaveService;
+
+public class test {
+
+	public static void main(String[] args) {
+		FileSaveService fss= new FileSaveService(); 
+		fss.FileReader("F:/gittest/plugins.zip");
+	}
+
+}
